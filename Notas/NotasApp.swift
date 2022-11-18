3e40811284a6dfe8e6c20e458131378c7300f296
Luis Angel Torres G <t.g.luisangel@hotmail.com>
2022-11-18 02:40:31 -0600

    Initial Commit

---
+++ Notas/NotasApp.swift
@@ -0,0 +1,20 @@
+//
+//  NotasApp.swift
+//  Notas
+//
+//  Created by Luis Angel Torres G on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NotasApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

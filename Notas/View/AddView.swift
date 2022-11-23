e3b5885c9912bbfbb0af2930ff53715bdcce6516
Luis Angel Torres G <t.g.luisangel@hotmail.com>
2022-11-23 04:32:04 -0600

    first commit

---
+++ Notas/View/AddView.swift
@@ -0,0 +1,8 @@
+//
+//  AddView.swift
+//  Notas
+//
+//  Created by Luis Angel Torres G on 18/11/22.
+//
+
+import Foundation
---
+++ Notas/ViewModel/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  Notas
+//
+//  Created by Luis Angel Torres G on 18/11/22.
+//
+
+import Foundation
